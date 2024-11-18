51fb4a55e8c26eec2679369e4f9647ea996ac2d0
= <=>
2024-11-18 15:56:32 +0000

    Added modules for calulation of alpha, TI and Filtering based on IEC

--- main.py
+++ main.py
@@ -26,3 +26,13 @@ with open("./config.json") as conf_file:
 
 PMM_T03 = MetMast(name="PMM_T03", **conf["PMM"]["data"])
 PMM_T03.load_timeseries_from_folder()
+
+high_wind = ("v1_Avg", 100)
+low_wind = ("v4_Avg", 25)
+
+PMM_T03.calculate_alpha(low_wind, high_wind)
+PMM_T03.calculate_TI()
+
+PMM_T03_filter = conf["PMM"]["filter"]
+PMM_T03.filter_timeseries(PMM_T03_filter)
+breakpoint()
--- modules/schema.py
+++ modules/schema.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 from datetime import datetime
+import numpy as np
 import pandas as pd
 from pathlib import Path
+import re
 import logging
 from rich.logging import RichHandler
 from rich.prompt import Prompt
@@ -47,7 +49,7 @@ class MetMast(BaseModel):
             low_memory=False,
         )
         # self.timeseries = df
-        return df
+        return df.astype(np.float64)
 
     def load_timeseries_from_folder(self):
         """
@@ -65,11 +67,49 @@ class MetMast(BaseModel):
         except Exception as ex:
             log.error(f"{ex}")
 
-    def IEC_filtering(self):
+    def calculate_alpha(self, low_wind: (str, int), high_wind: (str, int)):
+        """
+        Calculate alpha (wind shear exponent) value of the wind shear
+        based on the formula Vzi = Vh*(Zi/H)^a
+        Return: None but will overwrite self.timeseries
+        """
+        df = self.timeseries
+        numerator = np.where(
+            df[high_wind[0]] > 0, np.log(df[low_wind[0]] / df[high_wind[0]]), np.nan
+        )
+        df["a"] = numerator / np.log(low_wind[1] / high_wind[1])
+
+        log.info(
+            f"Wind shear exponent has been calculated using {low_wind} and {high_wind}"
+        )
+
+        self.timeseries = df
+
+    def calculate_TI(self):
+        df = self.timeseries
+        columns = [i for i in df.columns if re.match("v._Avg", i)]
+        for column in columns:
+            stem = column.split("_")[0]
+            std = f"{stem}_Std"
+            avg = f"{stem}_Avg"
+            ti = f"{stem}_TI"
+            df[ti] = df[avg] / df[std]
+
+        log.info(
+            f"Turbulence Intensity has been calculated for the following measurements {columns}"
+        )
+
+        self.timeseries = df
+
+    def filter_timeseries(self, Filter):
         """
         Filtering met mast dataset based on IEC 61400-12-1 spesifications for site calibrations
         1 (NumSamplesInterval_avg_PMM2 == 600)
-        2 (NumSamplesInterval_avg_CMM2 == 600)
+        2 (DirNumSamples > 595)
+        3 (Temperature > 2 C & Humidity < 80%)
+        4 (Mean wind speed <= 4 & >= 16 m/s)
+        5 (Measurement sector <=206.6 >=6.9
+
         3 (V1_avg_CMM2_cor >= 4 & V1_avg_CMM2_cor <= 16)
         4 (Dir1_avg_PMM2 >= 206.6 | Dir1_avg_PMM2 <= 6.9)
         5 (inflow_angle_PMM >= -2 & inflow_angle_PMM <= 2)
@@ -77,7 +117,51 @@ class MetMast(BaseModel):
         7 (alpha_V1_V3_PMM >= 0 & alpha_V1_V3_PMM <= 0.5)
         8 (Precipitation_avg_PMM2 < 10)
         """
-        pass
+
+        NumSamplesInterval = Filter["NumSamplesInterval"]
+        WindAvgMin = Filter["WindAvgMin"][1]
+        WindAvgMax = Filter["WindAvgMax"][1]
+        DirAvgMin = Filter["DirAvgMin"][1]
+        DirAvgMax = Filter["DirAvgMax"][1]
+        DirNumSamples = Filter["DirNumSamples"]
+        TempMin = Filter["TempMin"]
+        # InflowAngleMin = Filter["InflowAngleMin"]
+        # InflowAngleMax = Filter["InflowAngleMax"]
+        # TurbulanceIntencityMin = Filter["TurbulanceIntencityMin"]
+        # TurbulanceIntencityMax = Filter["TurbulanceIntencityMax"]
+        # AlphaMin = Filter["AlphaMin"]
+        # AlphaMax = Filter["AlphaMax"]
+        # PrecipitationMax = Filter["PrecipitationMax"]
+
+        df = self.timeseries
+
+        wind_col = Filter["WindAvgMin"][0]
+        dir_col = Filter["DirAvgMin"][0]
+        dir_columns = [i for i in df.columns if re.match("dir._num", i)]
+        # wind_columns = [i for i in df.columns if re.match("v._Avg", i)]
+
+        df["filter"] = 0
+
+        # NOTE this is number 1 filtering
+        df.loc[df["NumSamplesInterval"] < NumSamplesInterval, "filter"] = 1
+
+        # NOTE this is number 2 filtering
+        for dir_column in dir_columns:
+            df.loc[df[dir_column] < DirNumSamples, "filter"] = 2
+
+        # NOTE this is number 3 filtering
+        df.loc[df["thb_t_Avg"] < TempMin, "filter"] = 3
+
+        # NOTE this is number 4 filtering
+        df.loc[
+            ((df[wind_col] < WindAvgMin) | (df[wind_col] > WindAvgMax)), "filter"
+        ] = 4
+
+        # NOTE this is number 5 filtering
+        df.loc[((df[dir_col] < DirAvgMin) | (df[dir_col] > DirAvgMax)), "filter"] = 5
+
+        log.info(f"Filtering Base IEC has been applied {Filter}")
+        self.timeseries = df
 
 
 class Site(BaseModel):
